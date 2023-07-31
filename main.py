73a16845f45e03597bbf877f0c7545f2adc5a6bd
ggferreira <ggferreira@mpes.mp.br>
2023-07-31 15:48:30 -0300

    metodo post

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ app = FastAPI()
 
 usuarios = [(1, 'caio', 'minhasenha1'), (2, 'joao', 'minhasenha2')]
 
-@app.get("/teste/{id}")
-def main(id: int):
+@app.post("/usuario")
+def main(nome):
     for i in usuarios:
-        if i[0] == id:
-            return i[0]
-    return {'valor': id *10}
+        if i[1] == nome:
+            return i
+    return "Não existe esse usuário"
